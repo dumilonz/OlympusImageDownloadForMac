ca193579e4822511fcab5842aa51d8a5b6cbb5c1
Elke van der Walt <elkevanderwalt@Elkes-MacBook-Air.local>
2020-03-16 14:47:25 +0700

    Create list with new images & movies to download

--- odown.py
+++ odown.py
@@ -1,17 +1,37 @@
 from urllib.request import urlopen, urlretrieve
 from bs4 import BeautifulSoup
 import html5lib
+URL = "http://192.168.0.10/DCIM/100OLYMP/"
+array_start = "wlansd = new Array();"
+array_end = "wlansd.sort(cmptime);"
+already_downloaded = open('./downloads/downloadList.txt', 'r+').read()
+update_downloads = open('./downloads/updatedDownloads.txt', 'a+')
+
 def main():
     try:
         soup = BeautifulSoup(urlopen(URL), 'html5lib')
-        redirect_script = str(soup.find_all('script'))
+        display_script = str(soup.find_all('script'))
         print('Connected to ', URL)
 
+        download_array = get_download_array(display_script)
+        print('Retrieved ' + str(len(download_array)) + ' pictures to download...')
     except ConnectionRefusedError:
         print('ERROR: Make sure you\'re connmected to the Olympus WiFi and rerun ')
         return 0
 
     return 1
 
+def get_download_array(display_script):
+    photo_array = display_script[display_script.index(array_start)+len(array_start):redirect_script.index(array_end)]
+    array_size = int(photo_array[photo_array.rindex('[') + 1 :photo_array.rindex(']')]) + 1
+    wlansd = [None] * array_size
+    exec(photo_array)
+    
+    new_downloads = []
+    for el in wlansd:
+        filename = el[el.index(',') + 1: el.find(',', el.find(',') + 1)]
+        if '.ORF' not in filename and filename not in already_downloaded:
+            new_downloads.append(filename)
+    return new_downloads
 if __name__ == '__main__':
     exit(main())
