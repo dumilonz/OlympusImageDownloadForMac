936059eb954582f20ad4f41d03001f0be34791cf
Elke van der Walt <elkevanderwalt@Elkes-MacBook-Air.local>
2020-03-16 14:48:58 +0700

    Download images from each image's URL

--- odown.py
+++ odown.py
@@ -1,6 +1,8 @@
 from urllib.request import urlopen, urlretrieve
 from bs4 import BeautifulSoup
 import html5lib
+from datetime import datetime
+
 URL = "http://192.168.0.10/DCIM/100OLYMP/"
 array_start = "wlansd = new Array();"
 array_end = "wlansd.sort(cmptime);"
@@ -15,6 +17,10 @@ def main():
 
         download_array = get_download_array(display_script)
         print('Retrieved ' + str(len(download_array)) + ' pictures to download...')
+
+        download_pictures(download_array)
+        update_downloads.close()
+        print('Done...')
     except ConnectionRefusedError:
         print('ERROR: Make sure you\'re connmected to the Olympus WiFi and rerun ')
         return 0
@@ -33,5 +39,24 @@ def get_download_array(display_script):
         if '.ORF' not in filename and filename not in already_downloaded:
             new_downloads.append(filename)
     return new_downloads
+
+def download_pictures(image_array):
+    i = 1
+    num_downloads = len(image_array)
+    print('Starting download...')
+    for image_name in image_array:
+        try:
+            print(str(i) + 'of ' + str(num_downloads) + ': ' + image_name)
+            f = open("./downloads/" + image_name, 'wb')
+            f.write(urlopen(URL + image_name).read())
+            f.close()
+            i = i + 1
+            update_downloads.write(str(datetime.now()) + ' - ' + image_name + '\n')
+            
+        except ConnectionRefusedError:
+            print('ERROR: Make sure you\'re connmected to the Olympus WiFi. Run 2nd script and rerun ')
+        except: 
+            print('ERROR: could not download', image_name, '\n')
+
 if __name__ == '__main__':
     exit(main())
